def6aabe99836ed5ffcdb49b3dbb9f12bbf1b46b
alexshwe1 <alexander.shwe@gmail.com>
2024-02-28 19:10:35 -0600

    about paragraph

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,12 @@
+import { useRef } from 'react';
 import headshot from '../images/ headshot.png'
-import { motion, AnimatePresence, MotionConfig } from 'framer-motion';
+import { motion, AnimatePresence, useInView } from 'framer-motion';
 
 export default function Index() {
+
+  const ref = useRef(null)
+  const isInView = useInView(ref)
+
   return (
     <div className="flex flex-col">
       <div className="bg-gray-50 border-b-1 border-gray-300">
@@ -76,9 +81,25 @@ export default function Index() {
           </AnimatePresence>
         </div>
       </div>
-      <div className="bg-gray-50 border-b-1 border-gray-300">
-        <div className="py-24 px-5 md:py-48 md:px-10 flex flex-col items-center">
-          
+      <div className="py-24 px-5 md:py-48 md:px-52 bg-gray-50 border-b-1 border-gray-300">
+        <div 
+          className="flex flex-col items-center"
+        >
+          <div
+            ref={ref}
+            style={{
+              transform: isInView ? "none" : "translateY(150px)",
+              opacity: isInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+            }}
+          >
+            Hey I'm Alex Shwe and I love to build. My love for creating started with my middle school woodowrking elective. 
+            I loved learning about new tools and soon built a sizable shop of my own. Youtube was (and still is) my goto resource for exposing myself to new tools, 
+            and as soon as I discovered a new tool, I had to have it. The problem? Tools worth hundreds of dollars are pretty tough for a middle schooler to afford. 
+            In high school, I began learning to code and immidiately took to it. Instead of having to pay hundreds of dollars for a new tool, 
+            all I had to spend were a few seconds installing a package or library. Ever since, I have loved discovering new tools to put in my toolbelt.
+            
+          </div>
         </div>
       </div>
     </div>
